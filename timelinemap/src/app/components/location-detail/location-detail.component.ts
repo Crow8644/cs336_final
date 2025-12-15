d8a2a9464b9a7d42cb490970193ee226c19e8a82
emb23 <122387414+emb23@users.noreply.github.com>
2025-12-15 12:59:15 -0500

    basic styling

--- timelinemap/src/app/components/location-detail/location-detail.component.ts
+++ timelinemap/src/app/components/location-detail/location-detail.component.ts
@@ -5,9 +5,11 @@ import { PinsService, Pin } from '../../services/pins.service';
 import { LocationEditingComponent } from '../location-editing/location-editing.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-import { ReactiveFormsModule, Validators, FormGroup, FormControl, AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { ReactiveFormsModule, Validators, FormGroup, FormControl, AbstractControl, ValidationErrors, ValidatorFn, } from '@angular/forms';
+import { MatFormFieldModule} from '@angular/material/form-field';
 import {TextFieldModule} from '@angular/cdk/text-field';
 import { toObservable } from '@angular/core/rxjs-interop';
+import { MatInputModule} from '@angular/material/input'
 
 /*TODO:
 -Connect service
@@ -25,44 +27,53 @@ interface editForm {
 //https://dev.to/this-is-angular/how-to-combine-add-and-edit-forms-in-angular-4888
 @Component({
   selector: 'app-location-detail',
-  imports: [MatExpansionModule, LocationEditingComponent, MatButtonModule, MatIconModule, ReactiveFormsModule, TextFieldModule],
+  imports: [MatExpansionModule, LocationEditingComponent, MatButtonModule, MatIconModule, ReactiveFormsModule, TextFieldModule, MatFormFieldModule, MatInputModule],
   template: `
 <!-- I orginally had the entire expansion pannel in the @if and @else, but that was causing issues, ChatGPT suggested only putting the form parts in @if and @else
 <!-- The form for editing the location -->
-
   <mat-expansion-panel [expanded]="open()">
         <mat-expansion-panel-header>
           <mat-panel-title>
                 {{data().name}}    
           </mat-panel-title>
-
         </mat-expansion-panel-header>
       
-      <!-- So you can only edit if you aren't editing -->
-      @if(!editing){
-        <button matIconButton id = "edit" (click) = "edit()" type="button">
-        <mat-icon>edit</mat-icon>
-        </button>
-      }
       @if(editing){
       <form [formGroup]="editingForm" (ngSubmit)="onSubmit()">
-          <input type="text" formControlName="locationName" placeholder="Location Name"> <!-- CONNECT SERVICE HERE --> 
-          <input type="text" formControlName="startDate" placeholder="Start Date"> <!-- CONNECT SERVICE HERE --> 
-          <input type="text" formControlName="endDate" placeholder="End Date"> <!-- CONNECT SERVICE HERE --> 
+          <mat-form-field appearancew = 'fill'>
+            <mat-label>Location Name</mat-label>
+            <input type="text" formControlName="locationName" placeholder="Location Name" matInput> <!-- CONNECT SERVICE HERE --> 
+          </mat-form-field>
+
+          <mat-form-field>
+            <mat-label>Start Date</mat-label>
+            <input type="text" formControlName="startDate" placeholder="Start Date" matInput> <!-- CONNECT SERVICE HERE -->
+          </mat-form-field>
+
+          <mat-form-field>
+            <mat-label>End Date</mat-label>
+            <input type="text" formControlName="endDate" placeholder="End Date" matInput> <!-- CONNECT SERVICE HERE --> 
+          </mat-form-field>
 
           <!-- Change to textarea to allow box to show all of the description -->
-          <textarea MatInput 
-          formControlName="description"  
-          ngDefaultControl 
-          cdkTextareaAutosize
-          #autosize="cdkTextareaAutosize"
-          cdkAutosizeMinRows="2"
-          cdkAutosizeMaxRows="7"
-          > 
-          </textarea>
-          <!-- CONNECT SERVICE HERE --> 
 
-          <button type="submit">Save</button>
+          <mat-form-field>
+            <mat-label>Description</mat-label>
+            <textarea MatInput 
+            formControlName="description"  
+            ngDefaultControl 
+            cdkTextareaAutosize
+            #autosize="cdkTextareaAutosize"
+            cdkAutosizeMinRows="2"
+            cdkAutosizeMaxRows="7"
+            matInput
+            > 
+            </textarea>
+          </mat-form-field>
+          <!-- CONNECT SERVICE HERE --> 
+          <br>
+          <button type="submit" mat-mini-fab>Save</button>
+          <!-- So you can only edit if you aren't editing -->
       </form>
       }
 
@@ -70,10 +81,26 @@ interface editForm {
         <p>{{data().startTime}} - {{data().endTime}}</p> <!-- CONNECT SERVICE HERE -->
         <p>{{data().description}}</p> <!-- CONNECT SERVICE HERE -->
         }
+
+      @if(!editing){
+        <button matIconButton id = "edit" (click) = "edit()" type="button" mat-mini-fab>
+        <mat-icon>edit</mat-icon>
+        </button>
+      }
     </mat-expansion-panel>
     
   `,
-  styles: ``
+  styles: `
+    mat-expansion-panel {
+    color: rgb(220, 225, 233);
+    background-color: rgb(68, 73, 83);
+    }
+
+    mat-panel-title {
+      color: rgb(220, 225, 233);;
+    }
+  
+  `
 })
 export class LocationDetailComponent {
   service = inject(PinsService);
